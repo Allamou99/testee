aa641e5558af4cf68ad7fd2b2638749ebeff87cc
Soufiane Allamou <soufianeallamou99@outlook.fr>
2021-02-23 11:27:30 +0100

    first-spring-config

---
+++ src/com/javaa/springdemo/Coach.java
@@ -0,0 +1,5 @@
+package com.javaa.springdemo;
+
+public interface Coach {
+	public String getDailyWorkout();
+}
---
+++ src/com/javaa/springdemo/FootballCoach.java
@@ -0,0 +1,10 @@
+package com.javaa.springdemo;
+
+public class FootballCoach implements Coach {
+	
+	
+	@Override
+	public String getDailyWorkout(){
+		return "You gotta strike when you get closer to the goal";
+	}
+}
